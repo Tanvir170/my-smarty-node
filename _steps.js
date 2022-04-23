47f8278e38b9fb8de574ab0370c8566bf32746b3
Your Name <you@example.com>
2022-04-23 15:18:56 +0600

    added steps

---
+++ _steps.js
@@ -0,0 +1,27 @@
+/* 
+----------------------
+Basic server setup
+----------------------
+1. create a folder (manually or mkdir)
+2. open command line to chat folder.
+3. npm start
+4. npm install express
+5. Create index.js file
+----------------------
+For express 
+----------------------
+1. require express
+2. create app variable 
+3. declare port
+4. set app.get('/)
+5. listen to port 
+6. node index.js
+7. check your browser for that port
+---------------------
+get
+---------------------
+1. cors middleweare
+----------------------
+post
+---------------------
+*/
